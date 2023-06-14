1e57fd41bb5bb5fed635fc73cb07bd2ede181ac7
Khang Q.NGUYEN <nguyenquockhang117@gmail.com>
2023-06-14 15:09:43 +0200

    add ios Project

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Quoc Nguyen, Khang on 14/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
